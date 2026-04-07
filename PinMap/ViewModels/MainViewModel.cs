7f26690c60a4523974039720cc68efd020c4bf82
신 문섭 <admin@SolutionPart01.onmicrosoft.com>
2026-04-07 10:30:31 +0900

    4.7

--- PinMap/ViewModels/MainViewModel.cs
+++ PinMap/ViewModels/MainViewModel.cs
@@ -28,6 +28,8 @@ public partial class MainViewModel : ObservableObject
         }
     }
 
+    [ObservableProperty] private Brush _bC;
+
     private readonly PointDataLoader _dataLoader;
 
     public MainViewModel()
@@ -37,13 +39,31 @@ public partial class MainViewModel : ObservableObject
 
         // 데이터 처리 엔진을 통해 파일을 로드하고 Points 컬렉션에 대입합니다.
         // 파일 경로 "TextFile.txt"는 실행 파일 위치 기준입니다.
-        Points = _dataLoader.LoadPointsFromFile("Points_50k.txt");
+        Points = _dataLoader.LoadPointsFromFile("Sample.txt");
     }
 
     [RelayCommand]
     private void Test()
     {
         ChannelColors = ColorGenerator.GenerateRandomColors();
+    }
+
+    [RelayCommand]
+    private void C1()
+    {
+        BC = Brushes.White;
+    }
+
+    [RelayCommand]
+    private void C2()
+    {
+        BC = Brushes.DarkGray;
+    }
+
+    [RelayCommand]
+    private void C3()
+    {
+        BC = Brushes.Gray;
     }
 }
 
